82d56da17e81db1f5f0950e0d89cbb0c6ac6ee59
Edwin Körner <edwin.korner@medieinstitutet.se>
2022-11-18 09:43:45 +0100

    Small changes

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-on:click="$emit('call-method',cardProperties.scrollFunction)" :id="cardProperties.id" class="flex text-text-color justify-around text-center bg-container-color h-1/6 w-2/12 rounded-3xl shadow-lg m-4 cursor-pointer ease-in-out duration-500 hover:bg-container-hover-color opacity-95">
+    <div v-on:click="$emit('call-method',cardProperties.scrollFunction)" :id="cardProperties.id" class="flex text-text-color justify-around text-center bg-container-color w-2/12 rounded-3xl shadow-lg m-4 cursor-pointer ease-in-out duration-500 hover:bg-container-hover-color opacity-95">
         <div class="py-6">
             <h2>{{cardProperties.title}}</h2>
             <font-awesome-icon class="h-12 my-4" :icon="cardProperties.icon" />
